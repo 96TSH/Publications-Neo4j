8734ef33cc536f1ae1188dc1c211e80c3c2c140f
96TSH <tansiheng96@gmail.com>
2024-05-02 02:31:37 +0800

    final commit as of 02/05/24

--- Backend/neopub/settings.py
+++ Backend/neopub/settings.py
@@ -22,73 +22,86 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lkalealk7%6_(8#y7(d@w32kqg_4ck=6m%v)q+9ybp4j9p(*z#'
+SECRET_KEY = "django-insecure-lkalealk7%6_(8#y7(d@w32kqg_4ck=6m%v)q+9ybp4j9p(*z#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 uri = "bfe7dc5d.databases.neo4j.io:7687"
 username = "neo4j"
 password = "cRSR4La87jlVdeVel8XuK92yn0IqtKguNGlAzHr502s"
 
-config.DATABASE_URL = 'neo4j+s://{}:{}@{}'.format(username, password, uri)
+config.DATABASE_URL = "neo4j+s://{}:{}@{}".format(username, password, uri)
 config.ENCRYPTED_CONNECTION = True
 config.MAX_POOL_SIZE = 50
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "corsheaders",
     # 'django.contrib.staticfiles',
-    'django_neomodel',
-    'server',
+    "django_neomodel",
+    "server",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'neopub.urls'
+CORS_ALLOWED_ORIGINS = [
+    "https://neopub.netlify.app",
+    "http://178.128.16.149",
+    "http://localhost:4173",
+    "http://localhost:5173"
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ALLOW_ALL_ORIGINS = True
+
+ROOT_URLCONF = "neopub.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'neopub.wsgi.application'
+WSGI_APPLICATION = "neopub.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -98,16 +111,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -115,9 +128,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -136,4 +149,4 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 
 const Dashboard = () => {
   const neoDashUrl =
-    "http://neodash.graphapp.io/?share&type=database&id=5ce81c80-14a2-4d8f-9017-bd31a960711b&dashboardDatabase=neo4j&credentials=neo4j%2Bs%3A%2F%2Fneo4j%3AteV3BKUHpbDE-OrXCj-5O5SEn1znxzpin_BNubXEGoU%40%3A3038c058.databases.neo4j.io%3A76";
+    "http://neodash.graphapp.io/?share&type=database&id=5ce81c80-14a2-4d8f-9017-bd31a960711b&dashboardDatabase=neo4j&credentials=neo4j%2Bs%3A%2F%2Fneo4j%3AteV3BKUHpbDE-OrXCj-5O5SEn1znxzpin_BNubXEGoU%40%3A3038c058.databases.neo4j.io%3A7687&standalone=Yes";
 
   return (
     <Box
